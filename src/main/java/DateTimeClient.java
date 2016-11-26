21834db136deacf7b7df50e1a45a9eec8e087604
Ueli Kunz <kunz@ideadapt.net>
2016-11-26 12:00:28 +0100

    date time client with maven

---
+++ src/main/java/DateTimeClient.java
@@ -0,0 +1,45 @@
+import java.net.*;
+import java.io.*;
+
+class DateTimeClient {
+    public static void main(String[] args) {
+        String hostName = "";  // Rechner-Name bzw. -Adresse
+        int port;              // Port-Nummer
+        Socket c;       // Socket fuer die Verbindung zum Server
+
+        try {
+            hostName = args[0];
+            port = Integer.parseInt(args[1]);
+            c = new Socket(hostName, port);
+
+            BufferedReader vomServer = new BufferedReader(
+                    new InputStreamReader(
+                            c.getInputStream()));
+            PrintWriter zumServer = new PrintWriter(
+                    c.getOutputStream(),true);
+
+            BufferedReader vonTastatur = new BufferedReader(
+                    new InputStreamReader(
+                            System.in));
+
+            // Protokoll abwickeln
+            System.out.println("Server " + hostName +":"+ port + " sagt:");
+            String text = vomServer.readLine(); // vom Server empfangen
+            System.out.println(text);         // auf die Konsole schreiben
+            text = vonTastatur.readLine();    // von Tastatur lesen
+            zumServer.println(text);          // zum Server schicken
+            text = vomServer.readLine();      // vom Server empfangen
+            System.out.println(text);         // auf die Konsole schreiben
+
+            // Socket (und damit auch Stroeme) schliessen
+            c.close();
+        } catch (ArrayIndexOutOfBoundsException ae) {
+            System.out.println("Aufruf:");
+            System.out.println("java DateTimeClient <HostName> <PortNr>");
+        } catch (UnknownHostException ue) {
+            System.out.println("Kein DNS-Eintrag fuer " + hostName);
+        } catch (IOException e) {
+            System.out.println("IO-Error");
+        }
+    }
+}
